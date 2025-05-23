e159ed2055c962d5bc1aac824486d09ec3d2e170
brenno-ostemberg <brenno.ostemberg.dev@gmail.com>
2025-05-23 16:11:07 -0400

    feat: implementa classes das entidades do banco de dados

---
+++ app/src/main/java/com/example/atividadeavaliativa2_progmobile/database/entity/Jogador.java
@@ -0,0 +1,29 @@
+package com.example.atividadeavaliativa2_progmobile.database.entity;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+import androidx.room.ColumnInfo;
+import androidx.room.Index;
+
+@Entity(
+    indices = {@Index(value = {"nickname"}, unique = true)}
+)
+public class Jogador {
+
+    @PrimaryKey(autoIncrement = true)
+    public int idJogador;
+
+    @ColumnInfo(name = "nome")
+    public String nome;
+
+    @ColumnInfo(name = "nickname")
+    public String nickname;
+
+    @ColumnInfo(name = "email")
+    public String email;
+
+    @ColumnInfo(name = "dataNascimento")
+    public String dataNascimento;
+
+}
+
