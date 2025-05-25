8e3c22ea548af612b86ef8acdb6077912471dcc7
brenno-ostemberg <brenno.ostemberg.dev@gmail.com>
2025-05-25 12:39:11 -0400

    feat: adiciona DAO para as entidades 'Jogador' e 'Partida'

---
+++ app/src/main/java/com/example/atividadeavaliativa2_progmobile/database/dao/JogadorDao.java
@@ -0,0 +1,35 @@
+package com.example.atividadeavaliativa2_progmobile.database.dao;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Update;
+import androidx.room.Query;
+import com.example.atividadeavaliativa2_progmobile.database.entity.Jogador;
+import java.util.List;
+
+@Dao
+public interface JogadorDao {
+
+    @Delete
+    void excluiJogador(Jogador jogador);
+
+    @Insert
+    void insereJogador(Jogador jogador);
+
+    @Update
+    void atualizaJogador(Jogador jogador);
+
+    @Query("SELECT * FROM Jogador WHERE idJogador = :idJogador LIMIT 1")
+    Jogador encontreJogadorPorId(int idJogador);
+
+    @Query("SELECT * FROM Jogador WHERE nickname = :nickname LIMIT 1")
+    Jogador encontreJogadorPorNickName(String nickname);
+
+    @Query("SELECT * FROM Jogador ORDER BY nome ASC")
+    List<Jogador> selecionaTodosJogadores();
+
+    @Insert
+    long insereJogadorEPegaId(Jogador jogador);
+
+}
